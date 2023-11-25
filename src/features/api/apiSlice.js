58735dee01cafe9f9a1c5318b27cd45bcb29698c
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-25 11:14:26 +0600

    implements login persistency and solve prepareHeaders error

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,15 +1,15 @@
-/* eslint-disable no-undef */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.REACT_APP_API_URL,
-    prepareHeaders: async (headers, { getState, endPoint }) => {
-      const token = getState()?.auth()?.accessToken;
+    prepareHeaders: async (headers, { getState, endpoint }) => {
+      const token = getState()?.auth?.accessToken;
       if (token) {
         headers.set("Authorization", `Bearer ${token}`);
       }
+
       return headers;
     },
   }),
